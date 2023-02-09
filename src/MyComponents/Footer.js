5ca98426c24a5444b03c0cf3f615b97947e7604e
souvikkoley9674 <113795422+souvikkoley9674@users.noreply.github.com>
2023-02-09 13:49:43 +0530

    initial commit

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-dark text-light '>
+      <div className="text-center my-3">Copyright &copy; MyTodoList.com</div>
+    </footer>
+  )
+}
+
+export default Footer
